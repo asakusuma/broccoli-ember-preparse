9d52315e3cb970ab494039ee986f8f7816e4855f
asakusuma <asa.kusuma@gmail.com>
2016-02-09 09:53:49 -0800

    Get plugin working end to end

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-var Plugin = require('broccoli-caching-writer');
-var path = require('path');
-var stew = require('broccoli-stew');
-var walkSync = require('walk-sync');
-var fs = require('fs');
-
-EmberPreparse.prototype = Object.create(Plugin.prototype);
-EmberPreparse.prototype.constructor = EmberPreparse;
-function EmberPreparse(inputNodes, options) {
-  options = options || {};
-  Plugin.call(this, inputNodes, {});
-  this.options = options;
-}
-
-EmberPreparse.prototype.build = function() {
-  console.log(this.inputPaths);
-
-  var srcDir = this.inputPaths[0];
-  var destDir = this.outputPath;
-  var paths = walkSync(srcDir);
-  paths.forEach(function(target) {
-    var outPath = path.join(destDir, target);
-    var inPath = path.join(srcDir, target);
-    var content = fs.readFileSync(inPath, 'utf8');
-
-    fs.writeFileSync(outPath, 'swag' + content);
-  });
-};
+var DefineRewriter = require('./lib/define-rewriter/src/index');
 
 module.exports = function(node) {
-  node = stew.debug(node, {
-    name: 'preparse'
-  });
-  return new EmberPreparse([node]);
+  return new DefineRewriter(node);
 }
