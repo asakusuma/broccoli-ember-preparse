47080fed888807c56bb8c4c2465c942a15afb008
Kris Selden <kris.selden@gmail.com>
2016-08-01 12:13:16 -0700

    Add glimmer option and make more configurable

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var DefineRewriter = require('./lib/define-rewriter/src/index');
 var merge = require('broccoli-merge-trees');
 var removeFile = require('broccoli-file-remover');
 
-module.exports = function(distNode, loaderNode) {
-  var node = new DefineRewriter(merge([distNode, loaderNode]));
+module.exports = function(distNode, loaderNode, options) {
+  var input = merge([distNode, loaderNode])
+  var node = new DefineRewriter(input, options);
   return removeFile(node, {
     path: 'loader'
   });
 }
+
+module.exports.DefineRewriter = DefineRewriter;
