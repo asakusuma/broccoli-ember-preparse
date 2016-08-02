d579472a0bf85d831ebdf0398df76e8f1d6755d0
Kris Selden <kris.selden@gmail.com>
2016-08-01 22:22:26 -0700

    loader stuff isn't actually used
    Currently this requires the special loader and it
    is not overridable.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var DefineRewriter = require('./lib/define-rewriter/src/index');
-var merge = require('broccoli-merge-trees');
-var removeFile = require('broccoli-file-remover');
 
-module.exports = function(distNode, loaderNode, options) {
-  var input = merge([distNode, loaderNode])
-  var node = new DefineRewriter(input, options);
-  return removeFile(node, {
-    path: 'loader'
-  });
+module.exports = function(input, options) {
+  return new DefineRewriter(input, options);
 }
 
 module.exports.DefineRewriter = DefineRewriter;
